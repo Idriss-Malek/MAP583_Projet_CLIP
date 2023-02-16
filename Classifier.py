84fb955b6c97f723a11a8f2eb71db5749b9d9e18
Idriss-Malek <idrissmalek80@gmail.com>
2023-02-16 13:39:52 +0100

    comments

--- Classifier.py
+++ Classifier.py
@@ -21,12 +21,18 @@ class Classifier:
         self.softmax_similarity_desc = None   #probabilities for descriptors
 
     def descriptors_fn(self):
+        '''
+        Method that will fill the self.descriptors dictionnary using a large language model such as gpt3
+        '''
         #for label in self.labels
             # Method code here with GPT
             #descriptors = [label + ' which is/has ' + descriptor for descriptor in descriptors]
             #self.descriptors[label] = descriptors
         raise NotImplementedError("This function is not implemented yet")
     def compute_threshold(self):
+        '''
+        Compute a threshold that allows us to distinguich between descriptors that are in the p
+        '''
         s=0
         for label in self.labels:
             s+= len(self.descriptors[label])
@@ -34,7 +40,11 @@ class Classifier:
         self.threshold = 0.5/s
         print(self.threshold)
     def similarity_fn(self, images_, label):
-        # Method code here
+        '''
+        :param images_: images to analyze
+        :param label: label to analyze
+        :return: similarity of the label to the images and similarities of each descriptor
+        '''
         single_image = False
         images = self.preprocess(images_).to(self.device)
         if len(images.shape) == 3:
@@ -45,16 +55,23 @@ class Classifier:
             logits_per_image, logits_per_descriptor = self.clip_model(images, token_desc)
             similarity = np.mean(logits_per_image.cpu().numpy(), axis=1)
             if single_image:
-                self.similarity.append(similarity.item())
+                self.similarity.append(similarity.item())  #store similarity
         return similarity, logits_per_image.numpy()
 
     def set_image(self, image):
+        '''
+        set image
+        '''
         self.image = image
         self.similarity_desc = None
         self.similarity = None
         self.softmax_similarity_desc = None
 
     def classify(self):
+        '''
+        Classify the image that is stored.
+        WARNING: Can only be used after storing an image with set_image
+        '''
         if self.image is None:
             print('Set Image first!')
             return
@@ -69,6 +86,10 @@ class Classifier:
             print('{}) {}: {}%'.format(i + 1, pair[0], pair[1]*100))
 
     def explain(self, label):
+        '''
+        Explain which descriptor of the label pertains to the image stored.
+        WARNING: Can only be used after using classify once on the image stored.
+        '''
         if self.similarity_fn is None:
             print('Classify first!')
             return
@@ -86,13 +107,20 @@ class Classifier:
             else:
                 print('\U0000274C {}: {}%'.format(descriptors[i], 100*self.softmax_similarity_desc[label][i]))  # red mark emoji
 
-    def reset(self):
+    def reset_image(self):
+        '''
+        Reset image and scores that are linked to this image
+        '''
         self.image = None
         self.similarity = None
         self.similarity_desc = None
         self.softmax_similarity_desc = None
 
     def reset_label(self, labels):
+        '''
+        Reset all the classifier (labels, reset threshold to default, delete image and scores)
+        '''
         self.labels = labels
         self.descriptors = {label: [] for label in labels}
-        self.reset()
+        self.threshold = 0.5
+        self.reset_image()
